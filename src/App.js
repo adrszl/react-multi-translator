84eae717a9daf19e096d4531efcc6b5b60c28f9e
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-07-02 21:13:21 +0200

    added API URLs to be used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
 
+/*
+API to be used:
+
+https://yandex.com/dev/translate/
+https://funtranslations.com/api/sindarin
+https://funtranslations.com/api/yoda
+https://funtranslations.com/api/groot
+
+https://cloud.google.com/apis/docs/client-libraries-explained
+*/
+
 function App() {
   return (
     <div className="App">
